627af017f54a7ecf9327c83d9c33915c1cf17802
sinon02 <yz01@mail.ustc.edu.cn>
2019-11-30 16:33:22 +0800

    AC ex4-ex

--- ex4/ex4-ex.cpp
+++ ex4/ex4-ex.cpp
@@ -2,42 +2,29 @@
 #include <vector>
 #include <set>
 #include <algorithm> 
-
-int FIND_MAX(std::vector<int> E, int M)
+#include <map>
+#define INF 1000000000
+int Judge_OK(int value,int M, std::vector<int> A)
 {
-	std::vector<int>::iterator min_it = std::min_element(E.begin(), E.end());
-	if (M == 0)
+	std::vector<int>::iterator pre = A.begin();
+	int num = 0;
+	for (std::vector<int>::iterator it = A.begin() + 1; it != A.end(); it++)
 	{
-		return *min_it;
+		if (*it - *pre < value)//delete it
+		{
+			num++;
+		}
+		else
+		{
+			pre = it;
+		}
+		if (num > M)
+		{
+			return 0;
+		}
 	}
-	if (min_it == E.begin())
-	{
-		*(min_it + 1) += *(min_it);
-		E.erase(min_it);
-		return FIND_MAX(E, M - 1);
-	}
-	else if (min_it == E.end()-1)
-	{
-		*(min_it - 1) += *(min_it);
-		E.erase(min_it);
-		return FIND_MAX(E, M - 1);
-	}
-	else
-	{
-		int max1, max2;
-		std::vector<int> E1(E);
-		int index = min_it - E.begin();
-		*(min_it + 1) += *(min_it);
-		E.erase(min_it);
-		max1 = FIND_MAX(E, M - 1);
-		min_it = E1.begin() + index;
-		*(min_it - 1) += *(min_it);
-		E1.erase(min_it);
-		max2 = FIND_MAX(E1, M - 1);
-		return std::max(max1, max2);
-	}
+	return 1;
 }
-
 int main()
 {
 	int N, M;
@@ -48,10 +35,31 @@ int main()
 		std::cin >> A[i];
 	}
 	std::sort(A.begin(), A.end());
-	std::vector<int> E;
+	int min = INF;
 	for (int i = 0; i < N - 1; i++)
 	{
-		E.push_back(A[i + 1] - A[i]);
+		if (A[i + 1] - A[i] < min)
+		{
+			min = A[i + 1] - A[i];
+		}
 	}
-	std::cout<<FIND_MAX(E, M);
+	int left = min;
+	int right = *(A.end() - 1)  -*(A.begin());
+	int result = 0;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (Judge_OK(mid, M, A))
+		{
+			result = mid;
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+	std::cout << result;
+	return 0;
+
 }
